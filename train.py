6f71e0f123db687dda1f2c189d71cced84aed433
Avi <aqbewtra@gmail.com>
2021-02-17 20:48:10 -0500

    CyclicLR test

--- train.py
+++ train.py
@@ -52,7 +52,8 @@ def main():
     #optimizer = lambda m: optim.SGD(m.parameters(), lr=lr, momentum=momentum, nesterov=nesterov, weight_decay=weight_decay)
     optimizer = lambda m: optim.Adam(m.parameters(), lr=lr, weight_decay=weight_decay)
     #lr_scheduler = lambda o: optim.lr_scheduler.CosineAnnealingLR(o, 8, eta_min=0, last_epoch=-1)
-    lr_scheduler = lambda o: optim.lr_scheduler.MultiStepLR(o, milestones=milestones, gamma=gamma)
+    lr_scheduler = lambda o: optim.lr_scheduler.CyclicLR(o, base_lr=0.001, max_lr=0.1,step_size_up=5,mode="exp_range",gamma=0.85)
+    #lr_scheduler = lambda o: optim.lr_scheduler.MultiStepLR(o, milestones=milestones, gamma=gamma)
 
     loss_fn = torch.nn.BCEWithLogitsLoss()
     #loss_fn = WeightedFocalLoss()
