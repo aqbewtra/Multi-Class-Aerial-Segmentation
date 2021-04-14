dff200c3199ac753026de057e0edda511d9ee1fe
Avi <aqbewtra@gmail.com>
2021-04-14 17:16:59 -0400

    IoU - developed jaccard distance class implementation

--- jaccard.py
+++ jaccard.py
@@ -1,34 +1,24 @@
 import torch.nn as nn
-import torch
-
 import torch.nn.functional as F
-
-from sklearn.metrics import jaccard_similarity_score as jsc
+import torch
+import torchvision.transforms as transforms
+import numpy as np
 
 class Jaccard(nn.Module):
     def __init__(self):
         super(Jaccard, self).__init__()
 
+    def intersect(self, img, label):
+        return torch.sum((img == label).to(dtype=torch.int64))
+
     def forward(self, img, label):
-        label = label.cpu().detach().numpy().reshape(-1)
-        img = img.cpu().detach().numpy().reshape(-1)
-        return jsc(img,label)
+        intersection = self.intersect(img, label)
+        union = img.shape[0]*img.shape[1] + label.shape[0]*label.shape[1] - intersection
+        return intersection / union
 
-
-
-def iou(pred, target, n_classes = 12):
-  ious = []
-  pred = pred.view(-1)
-  target = target.view(-1)
-
-  # Ignore IoU for background class ("0")
-  for cls in range(1, n_classes):  # This goes from 1:n_classes-1 -> class "0" is ignored
-    pred_inds = pred == cls
-    target_inds = target == cls
-    intersection = (pred_inds[target_inds]).long().sum().data.cpu()[0]  # Cast to long to prevent overflows
-    union = pred_inds.long().sum().data.cpu()[0] + target_inds.long().sum().data.cpu()[0] - intersection
-    if union == 0:
-      ious.append(float('nan'))  # If there is no ground truth, do not include in evaluation
-    else:
-      ious.append(float(intersection) / float(max(union, 1)))
-  return np.array(ious)
+    def to_tensor(self, label):
+        label = torch.as_tensor(np.array(label), dtype=torch.int64)
+        label = torch.transpose(label, 0,2)
+        label = transforms.functional.rotate(label, 270)
+        label = transforms.functional.hflip(label)
+        return label[:][0]
