2efcbe89215241443df87ce0ffa4ed4de079eaa6
Avi <aqbewtra@gmail.com>
2021-04-14 15:37:22 -0400

    Model Demo Prints IoU-Jaccard Distance

--- saved_models/model_demo.py
+++ saved_models/model_demo.py
@@ -8,6 +8,7 @@ currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 
+from jaccard import Jaccard
 import segm
 from single_channel_util import formatting
 from train import batch_size, num_workers
@@ -16,7 +17,9 @@ from dataset import SegmentationDataset
 import matplotlib.pyplot as plt
 from util import fix_labels
 
-model_path = 'model-13.pth'
+import numpy as np
+
+model_path = 'model-7.pth'
 
 dataset_root = '../data/dataset-sample/'
 img_dir = dataset_root + 'image-chips/'
@@ -31,36 +34,29 @@ def demo():
     #load model
     model = torch.load(model_path, map_location=torch.device('cpu'))
     model.eval()
-
-    #dataset
     dataset = SegmentationDataset(img_dir, label_dir, scale=1)
-    image = Image.open(dataset.path_dict[0][1]).convert('RGB')
+    label = Image.open(dataset.path_dict[0][1]).convert('RGB')
     loader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False, \
         num_workers=num_workers, pin_memory=torch.cuda.is_available())
 
-    #fig, axarr = plt.subplots(3,1)
-    #run an image(s) through
     with torch.set_grad_enabled(False):
         for batch_idx, (imgs, labels) in enumerate(loader):
             imgs, labels = map(lambda x: x.to(device, dtype=torch.float32), (imgs, labels))
-            #segm.tensor_to_image(imgs[0])
             prediction = model(imgs)
-            #convert out tensor to image
-            ####CHANGE######
-            # out_imgs = segm.tensor_to_image((prediction[0].unsqueeze_(0) * 40).squeeze_(0))
-            #out_imgs = formatting.to_three_channel(prediction[0].squeeze())
-            #out_imgs = fix_labels.
-
 
             out_imgs = torch.argmax(prediction[0], dim=0)
             print(out_imgs)
             prediction.detach()
             break
     print(prediction[0].unique())
+    jaccard = Jaccard()
+    
+    print("JACCARD:   ", jaccard(out_imgs, jaccard.to_tensor(label)/40))
 
+    
     f, axarr = plt.subplots(2,1)
     axarr[0].imshow(out_imgs)
-    axarr[1].imshow(image)
+    axarr[1].imshow(label)
     plt.show()
 
 
